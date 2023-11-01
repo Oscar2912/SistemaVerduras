e89a9f38bbc87d2a5f703c2e052e19a6b9f96ae9
Treaske <alexdblas@gmail.com>
2023-11-01 10:34:30 +0100

    Tester2 Comprobacion

--- src/Main.java
+++ src/Main.java
@@ -58,6 +58,20 @@ public class Main {
                             " Color: " + verduras[0].getColor() + " Calorias: " + verduras[0].getCalorias() +
                             " ¿Debe cocinarse? " + verduras[0].getDebeCocinarse());
 
+        //Prueba del funcionamiento de los métodos set
+
+        System.out.println("\n***** PRUEBA SETTER ******");
+
+        verduras[0].setId(34);
+        verduras[0].setNombre("Calabazin");
+        verduras[0].setCalorias(45);
+        verduras[0].setColor("Verde");
+        verduras[0].setDebeCocinarse("Si");
+
+        System.out.println("Id: " + verduras[0].getId() + " Nombre: " + verduras[0].getNombre() +
+                " Color: " + verduras[0].getColor() + " Calorias: " + verduras[0].getCalorias() +
+                " ¿Debe cocinarse? " + verduras[0].getDebeCocinarse());
+
 
     }
 }
